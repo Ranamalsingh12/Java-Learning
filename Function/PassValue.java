51ee189410e5a290ef6a08a9a2b78a226e5a1af1
Ranamalsingh12 <ranams99911@gmail.com>
2022-11-27 12:04:39 +0530

    pass value

--- Function/PassValue.java
+++ Function/PassValue.java
@@ -9,7 +9,17 @@ public class PassValue {
 
         System.out.println(a + " " + b);
         // The value will not get swapped because we have to understand how pass by value works
+        // lets take an example where the value will change 
+
+        int[] arr = {1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
     }
+
+    static void change(int[ num]) {
+        num[0] = 99;
+
+    }
+
 
     static void SwapNumber (int a , int b) {
         int temp = a;
