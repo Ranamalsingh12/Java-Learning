2b429de3154ffcba34f45034c1a0649b5dc1b118
Ranamalsingh12 <ranams99911@gmail.com>
2022-11-30 23:38:30 +0530

    input output of 2D array

--- ArrayandArraylist/Array2d.java
+++ ArrayandArraylist/Array2d.java
@@ -6,6 +6,26 @@ public class Switch {
 
         // let's now take a look at 2D arrays - it's kind of matrix. now lets take a look how its get defined.
         int[][] arr2D = new int[3][];
-        // we have to specify the number of rows first its mandatory 
+        // we have to specify the number of rows first its mandatory.
+        // You can imagine it as array within the array 
+
+        // Input 
+
+        for (int i = 0; i < arr2D.length; i++) {
+            // for each column in every row
+
+            for (j = 0; i < arr2D[i].length; i++ ) {
+                arr[i][j] = int.nextInt();
+            }
+        }
+
+        for (int i = 0; i < arr2D.length; i++) {
+            // for each column in every row
+
+            for (j = 0; i < arr2D[i].length; i++ ) {
+                System.out.println(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
