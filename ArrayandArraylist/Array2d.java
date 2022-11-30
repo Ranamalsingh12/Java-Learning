35522b0b2ee4d6e3afcc565e80837db244854518
Ranamalsingh12 <ranams99911@gmail.com>
2022-11-30 23:21:39 +0530

    2D arrays initial commit

--- ArrayandArraylist/Array2d.java
+++ ArrayandArraylist/Array2d.java
@@ -6,6 +6,6 @@ public class Switch {
 
         // let's now take a look at 2D arrays - it's kind of matrix. now lets take a look how its get defined.
         int[][] arr2D = new int[3][];
-        // we have to specify the number of rows first its mandatory
+        // we have to specify the number of rows first its mandatory 
     }
 }
