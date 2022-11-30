ceb6c9b32e5bc27daad3cbc3729fa92f3231a75e
Ranamalsingh12 <ranams99911@gmail.com>
2022-11-30 23:19:18 +0530

    2D array initial commit

---
+++ ArrayandArraylist/Array2d.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // let's now take a look at 2D arrays - it's kind of matrix. now lets take a look how its get defined.
+        int[][] arr2D = new int[3][];
+        // we have to specify the number of rows first its mandatory
+    }
+}
