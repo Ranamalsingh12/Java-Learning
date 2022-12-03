dd7891833ae0098d0e8763bf6405bb2baa6b7318
Ranamalsingh12 <ranams99911@gmail.com>
2022-12-03 22:27:26 +0530

    searching and sorting initialising

---
+++ Searching&Sorting/Searching/LinearSearch/LinearSearch.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+    }
+}
