86b0742592057f3459ae55298dba1ff202522d9d
Ranamalsingh12 <ranams99911@gmail.com>
2022-12-05 22:11:35 +0530

    binary search code

--- Searching&Sorting/Searching/BinarySearch/BinarySearch.java
+++ Searching&Sorting/Searching/BinarySearch/BinarySearch.java
@@ -2,8 +2,35 @@ import java.util.Scanner;
 
 public class BinarySearch {
     public static void main (String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
+        // Scanner input = new Scanner(System.in);
+        // int n = input.nextInt();
+
+        int[] arr = {-14,-4,0,13,19,36,40,54,65,100};
+        int target = 65;
+
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
         
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // int mid = (start+end)/2; it will cause error if we have so big start and end value
+
+            int mid = start + (end - start)/2;
+
+            if( target < arr[mid] ) {
+                end = mid - 1;
+            } else if ( target > arr[mid] ) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
     }
 }
